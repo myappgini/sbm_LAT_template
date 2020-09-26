2e4c195d25c047b8b8b66a43099f3d1d44a4232c
landinialejandro <landinialejandro@hotmail.com>
2020-09-26 13:07:29 -0300

    fix lay out

--- sideBarMenu_lat.php
+++ sideBarMenu_lat.php
@@ -6,7 +6,7 @@ if (!isset($Translation)) {
 }
 ?>
 <!-- aside.main-sidebar -->
-<aside class="main-sidebar elevation-4 sidebar-dark-primary" style=" z-index: 1050;">
+<aside class="main-sidebar elevation-4 sidebar-dark-primary" >
     <!-- Brand Logo -->
     <a href="<?php echo PREPEND_PATH; ?>index.php" class="brand-link">
         <img src="<?php echo PREPEND_PATH; ?><?php echo $LAT_globals['app-brand-icon']; ?>" alt="app-brand-icon" class="brand-image img-rounded elevation-3" style="opacity: .8">
